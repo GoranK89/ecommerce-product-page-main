fac2ba2acb87d9a2a4ddf9d9ba970a13073c2dc4
Goran Kolar <86071804+GoranK89@users.noreply.github.com>
2022-11-03 12:04:21 +0100

    Lightbox thumb functionality (no popup yet)

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+/* Cart functionality */
+const cartIcon = document.getElementById('cart-icon');
+
+cartIcon.addEventListener('click', () => {
+  cartIcon.insertAdjacentHTML(
+    'afterend',
+    `
+    <div id="cart-dropdown_wrapper">
+     <p>Cart</p>
+     <p>Your cart is empty</p>
+    </div>
+    `
+  );
+});
+
+/* Add active border on thumbnail images */
+const bigProductImg = document.querySelector('.big-product-img');
+const thumbnailWrapper = document.querySelector(
+  '.light-box-thumbnail_container'
+);
+const thumbnailProductImgs = document.querySelectorAll('.product-img-thumb');
+
+thumbnailProductImgs.forEach(img => {
+  img.addEventListener('click', () => {
+    thumbnailProductImgs.forEach(img => {
+      img.classList.remove('active-img-thumb');
+    });
+    img.classList.toggle('active-img-thumb');
+    bigProductImg.src = img.src.replace(/-thumbnail/g, '');
+  });
+});
